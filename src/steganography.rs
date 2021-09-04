97574dcd62f7cfe7c734d358c78e6d97f7dd8b2c
Ryan Jones-Ward <sciguyryan@gmail.com>
2021-09-04 13:14:31 +0100

    More refactoring work
    More refactoring work on the Steganography struct.

--- src/steganography.rs
+++ src/steganography.rs
@@ -8,15 +8,12 @@ use std::convert::TryFrom;
 
 #[derive(Debug)]
 pub struct Steganography {
-    version: Version,
     pub images: [DynamicImage; 2],
 }
 
 impl Steganography {
     pub fn new() -> Self{
         let mut v = Self {
-            version: Version::default(),
-
             // Create a dummy image for the two potential input images.
             // These will be replaced with the relevant method calls.
             images: [image::DynamicImage::new_bgr8(1, 1), image::DynamicImage::new_bgr8(1, 1)]
@@ -36,10 +33,15 @@ impl Steganography {
     /// * `output_path` - The output image file path.
     ///
     /// Note: When using this method the first image in the `images` array will be the reference image and the second will be the output image.
-    fn encode(&mut self, version: u32, input_path: &str, key: &str, plaintext: &str, output_path: &str) -> Result<()> {
-        if let Err(e) = self.set_version(version) {
-            return Err(e);
-        }
+    pub fn encode(&mut self, version: u32, input_path: &str, key: &str, plaintext: &str, output_path: &str) -> Result<()> {
+        let v = match Steganography::get_version(version) {
+            Ok(v) => {
+                v
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        };
 
         log::debug!("Loading (reference) image file @ {}", &input_path);
         match Steganography::load_image(input_path) {
@@ -51,7 +53,58 @@ impl Steganography {
             }
         }
 
+        // Replace the placeholder image with a container image for the output image.
+        let (w, h) =  self.images[0].dimensions();
+        image::DynamicImage::new_rgba16(w, h);
+
+        // Call the encoding function for the specified version.
+        match v {
+            Version::V0x01 => self.encode_v1(),
+        }
+    }
+
+    fn encode_v1(&mut self) -> Result<()> {
         Ok(())
+    }
+
+    /// Read encoded data from an image.
+    ///
+    /// # Arguments
+    ///
+    /// * `version` - The version of the encoding algorithm to use.
+    /// * `original_path` - The original image file path.
+    /// * `key` - The plaintext encryption key to be used.
+    /// * `encoded_path` - The encoded image file path.
+    ///
+    /// Note: When using this method the first image in the `images` array will be the reference image and the second will be the output image.
+    pub fn decode(&mut self, version: u32, original_path: &str, key: &str, encoded_path: &str) -> Result<&str> {
+        let v = match Steganography::get_version(version) {
+            Ok(v) => {
+                v
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        };
+
+        log::debug!("Loading (reference) image file @ {}", &original_path);
+        match Steganography::load_image(original_path) {
+            Ok(img) => {
+                self.images[0] = img;
+            },
+            Err(e) => {
+               return Err(e)
+            }
+        }
+
+         // Call the encoding function for the specified version.
+        match v {
+            Version::V0x01 => self.decode_v1(),
+        }
+    }
+
+    fn decode_v1(&mut self) -> Result<&'static str> {
+        Ok("")
     }
 
     fn load_image(file_path: &str) -> Result<DynamicImage> {
@@ -75,11 +128,10 @@ impl Steganography {
         }
     }
 
-    fn set_version(&mut self, version: u32) -> Result<()> {
+    fn get_version(version: u32) -> Result<Version> {
         match Version::try_from(version) {
             Ok(r) => {
-                self.version = r;
-                Ok(())
+                Ok(r)
             },
             Err(e) => Err(e)
         }
