754891c44638af952bf1d61e26618efd9a235894
Ryan Jones-Ward <sciguyryan@gmail.com>
2021-11-25 10:51:03 +0000

    Rework build.rs
    Rework build.rs.

--- psistega3-cli/build.rs
+++ psistega3-cli/build.rs
@@ -1,12 +1,14 @@
 #[cfg(windows)]
 extern crate winres;
 
-#[cfg(windows)]
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("..\\assets\\icon.ico");
-    res.compile().unwrap();
+    #[cfg(windows)]
+    windows_only();
 }
 
-#[cfg(unix)]
-fn main() {}
+#[cfg(windows)]
+fn windows_only() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("..//assets//icon.ico");
+    res.compile().unwrap();
+}
