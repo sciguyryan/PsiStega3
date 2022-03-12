873805ec31afccc08676bb36d9171bf7b128d3a6
Ryan Jones-Ward <sciguyryan@gmail.com>
2022-03-12 18:53:48 +0000

    Experimenting with icons, and Window controls
    Experimenting with embedded icons and Window controls. Nothing substancial yet.

--- psistega3-gui/src/main.rs
+++ psistega3-gui/src/main.rs
@@ -2,12 +2,24 @@
 
 #![windows_subsystem = "windows"]
 
-use fltk::{app, prelude::*, window::Window};
+use fltk::{app, prelude::*, window::Window, image};
 
 fn main() {
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut wind = Window::new(100, 100, 800, 600, "PsiStega 3 :: GUI");
+
+    let icon = icon();
+    wind.set_icon(Some(icon));
+
     wind.end();
     wind.show();
     app.run().unwrap();
+}
+
+fn icon() -> image::PngImage {
+    let bytes = include_bytes!("..\\..\\assets\\icon.png");
+    let ico = image::PngImage::from_data(bytes);
+
+    // TODO - error check this.
+    ico.unwrap()
 }
