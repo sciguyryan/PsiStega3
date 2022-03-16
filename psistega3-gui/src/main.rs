705d0f985aef762632d0e3d08379b11d3ed4fe09
Ryan Jones-Ward <sciguyryan@gmail.com>
2022-03-16 13:56:00 +0000

    Experimenting with GUIs.

--- psistega3-gui/src/main.rs
+++ psistega3-gui/src/main.rs
@@ -2,18 +2,62 @@
 
 #![windows_subsystem = "windows"]
 
-use fltk::{app, prelude::*, window::Window, image};
+use fltk::{app, prelude::*, window::Window, image, group::{Tabs, Group}, button, input, output};
+use fltk_theme::{ColorTheme, color_themes, WidgetTheme, ThemeType};
+
+const WIDTH: i32 = 550;
+const HEIGHT: i32 = 400;
 
 fn main() {
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 800, 600, "PsiStega 3 :: GUI");
 
-    let icon = icon();
-    wind.set_icon(Some(icon));
+    //let theme = ColorTheme::new(color_themes::GRAY_THEME);
+    //theme.apply();
+
+    let widget_theme = WidgetTheme::new(ThemeType::Greybird);
+    widget_theme.apply();
+
+    build_window();
+
+    app.run().unwrap();
+}
+
+fn build_window() {
+    let mut wind = Window::new(100, 100, WIDTH, HEIGHT, "PsiStega 3 :: GUI");
+
+    wind.set_icon(Some(icon()));
+
+    add_widgets();
 
     wind.end();
     wind.show();
-    app.run().unwrap();
+}
+
+fn add_widgets() {
+    let base_control_height = 25;
+
+    let tab = Tabs::new(10, 10, WIDTH - 20, HEIGHT - 20, "");
+
+    let grp1 = Group::new(10, 35, WIDTH - 20, HEIGHT - 45, "Encode\t\t");
+
+    // Input file
+    let mut y_pos = 50;
+    let input_encode = output::Output::new(100, y_pos, 300, base_control_height, "").with_label("Input Path: ");
+    let input_browse_encode = button::Button::new(WIDTH - 100, y_pos, 80, base_control_height, "...");
+
+    y_pos += base_control_height + 10;
+    let output_encode = output::Output::new(100, y_pos, 300, base_control_height, "").with_label("Output Path: ");
+    let output_browse_encode = button::Button::new(WIDTH - 100, y_pos, 80, base_control_height, "...");
+
+    y_pos += base_control_height + 10;
+    let output_encode = output::Output::new(100, y_pos, 300, base_control_height, "").with_label("Key: ");
+    let output_browse_encode = button::Button::new(WIDTH - 100, y_pos, 80, base_control_height, "...");
+
+    grp1.end();
+
+    let grp2 = Group::new(10, 35, WIDTH - 30, HEIGHT - 25, "Decode\t\t");
+    grp2.end();
+    tab.end();
 }
 
 fn icon() -> image::PngImage {
