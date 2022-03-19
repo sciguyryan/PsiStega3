1f5ecbb90853b8f80a87bdff1ebd74ac121564a3
Ryan Jones-Ward <sciguyryan@gmail.com>
2022-03-19 13:33:43 +0000

    Implement file selection dialog buttons
    Implement file selection dialog buttons.

--- psistega3-gui/src/main.rs
+++ psistega3-gui/src/main.rs
@@ -2,7 +2,7 @@
 
 #![windows_subsystem = "windows"]
 
-use fltk::{app, prelude::*, window::Window, image, group::{Tabs, Group}, button, input, output, enums::Color, menu};
+use fltk::{app, prelude::*, window::Window, image, group::{Tabs, Group}, button::Button, output::Output, menu::Choice, dialog::{FileDialog, FileDialogType, FileDialogOptions}};
 use fltk_theme::{WidgetTheme, ThemeType};
 
 const WIDTH: i32 = 550;
@@ -44,33 +44,39 @@ fn add_widgets() {
 
     // Input path.
     let mut y_pos = 50;
-    let mut input_encode = output::Output::new(100, y_pos, 370, base_control_height, "")
+    let mut input_encode = Output::new(100, y_pos, 370, base_control_height, "")
         .with_label("Input Path: ");
-    let mut input_browse_encode = button::Button::new(WIDTH - 70, y_pos, 30, base_control_height, "...");
-    input_browse_encode.set_callback(move |_| input_encode.set_value("Hello World!"));
+    let mut input_browse_encode = Button::new(WIDTH - 70, y_pos, 30, base_control_height, "...");
+    input_browse_encode.set_callback(move |_| {
+        let file_path = open_file_dialog();
+        input_encode.set_value(&file_path);
+    });
 
     // Output path.
     y_pos += base_control_height + spacer;
-    let mut output_encode = output::Output::new(100, y_pos, 370, base_control_height, "")
+    let mut output_encode = Output::new(100, y_pos, 370, base_control_height, "")
         .with_label("Output Path: ");
-    let mut output_browse_encode = button::Button::new(WIDTH - 70, y_pos, 30, base_control_height, "...");
-    output_browse_encode.set_callback(move |_| output_encode.set_value("Hello World!"));
+    let mut output_browse_encode = Button::new(WIDTH - 70, y_pos, 30, base_control_height, "...");
+    output_browse_encode.set_callback(move |_| {
+        let file_path = save_file_dialog();
+        output_encode.set_value(&file_path);
+    });
 
     // Version.
     y_pos += base_control_height + spacer;
-    let mut version_select_encode = menu::Choice::new(100, y_pos, 100, base_control_height, None)
+    let mut version_select_encode = Choice::new(100, y_pos, 100, base_control_height, None)
         .with_label("Version: ");
     version_select_encode.add_choice("1");
 
     // Set key.
     y_pos += base_control_height + spacer;
     let mut key = "";
-    let mut set_key = button::Button::new(100, y_pos, 100, base_control_height, "Set Key");
-    set_key.set_callback(move |_| key = "pineapples");
+    let mut set_key = Button::new(100, y_pos, 100, base_control_height, "Set Key");
+    set_key.set_callback(move |_| { key = "banana" });
 
     // Encode.
     y_pos += base_control_height + spacer;
-    let mut encode = button::Button::new(100, y_pos, 100, base_control_height, "Encode!");
+    let mut encode = Button::new(100, y_pos, 100, base_control_height, "Encode!");
     encode.set_callback(move |_| handle_encode());
 
     grp1.end();
@@ -86,4 +92,22 @@ fn handle_encode() {
 fn icon() -> image::PngImage {
     let bytes = include_bytes!("../../assets/icon.png");
     image::PngImage::from_data(bytes).expect("Error getting window icon data.")
+}
+
+fn open_file_dialog() -> String {
+    let mut dlg = FileDialog::new(FileDialogType::BrowseFile);
+    dlg.set_option(FileDialogOptions::NoOptions);
+    dlg.set_filter("PNG Files\t*.png");
+    dlg.show();
+
+    dlg.filename().to_string_lossy().to_string()
+}
+
+fn save_file_dialog() -> String {
+    let mut dlg = FileDialog::new(FileDialogType::BrowseSaveFile);
+    dlg.set_option(FileDialogOptions::SaveAsConfirm);
+    dlg.set_filter("PNG Files\t*.png");
+    dlg.show();
+
+    dlg.filename().to_string_lossy().to_string()
 }
